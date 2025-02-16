6a0c5cfd73a9baf3a848eaea605076f7b48ee6e0
Marcus Näslund <marcus.naslund@kognity.com>
2025-02-16 19:33:21 +0100

    update

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-const infoBox = document.querySelector("#info");
-const map = L.map("map").setView([20, 0], 2);
-const modal = document.getElementById("myModal");
-const modalFlag = document.querySelector(".modal-content img");
-const modalWikiLink = document.querySelector(".modal-content p#wiki a")
-const modalContent = document.querySelector(".modal-content p#content");
-const span = document.getElementsByClassName("close")[0];
+// ---
+// Internal logic
 
-let countryData = {};
-let highlightedCountries = {};
-let unHighlightedCountries = [];
+const fetchJson = async (filename) => {
+  const response = await fetch(filename);
+  const data = await response.json();
+  return data;
+};
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "&copy; OpenStreetMap contributors",
-}).addTo(map);
-
-const showModal = () => {
+// UI
+//
+const showModal = (type, data) => {
+  const modalContent = document.querySelector(`#modal-content-${type}`);
   modal.style.display = "block";
+  modalContent.classList.remove("invisible");
+
+  if (type === "one") {
+    const modalFlag = document.querySelector(".modal-content img");
+    const modalWikiLink = document.querySelector(".modal-content p#wiki a");
+    const modalText = document.querySelector("#modal-content-one p#content");
+    const { order, name, content, iso_a2 } = data;
+
+    modalWikiLink.href = `https://en.wikipedia.org/wiki/${name}`;
+    modalText.innerText = `(${order}: ${name})\n${content}`;
+    modalFlag.src = `https://flagsapi.com/${iso_a2}/flat/64.png`;
+  } else if (type === "two") {
+    // ...
+  } else if (type === "three") {
+    const modalText = document.querySelector("#modal-content-three p");
+    modalText.innerText = data;
+  }
 };
 
 const hideModal = () => {
   modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    hideModal();
-  }
+  document
+    .querySelectorAll(".modal-selector")
+    .forEach((e) => e.classList.add("invisible"));
 };
 
 const getStyle = (country, hover = false) => {
@@ -46,55 +56,76 @@ const getStyle = (country, hover = false) => {
   }
 };
 
+// ---
+
+const infoBox = document.querySelector("#info");
+const map = L.map("map").setView([20, 0], 2);
+const modal = document.getElementById("myModal");
+const list = document.querySelector("#list");
+const listContent = document.querySelector("#modal-content-two ul");
+const span = document.getElementsByClassName("close")[0];
+
+const countryData = await fetchJson("data.json");
+const worldData = await fetchJson("world.json");
+
+const highlightedCountries = new Set(Object.keys(countryData));
+const unHighlightedCountries = worldData.features
+  .map((feature) => feature.properties.name)
+  .filter((name) => !highlightedCountries.has(name));
+highlightedCountries.forEach((name) => {
+  const { order } = countryData[name];
+  const listItem = document.createElement("li");
+  listItem.innerText = `${order}: ${name}`;
+  listContent.appendChild(listItem);
+});
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    hideModal();
+  }
+};
+
+// List button
+document.querySelector("#showlist").addEventListener("click", () => {
+  showModal("two");
+});
+
+// New button
 document.querySelector("#go").addEventListener("click", () => {
   const randomElement =
     unHighlightedCountries[
       Math.floor(Math.random() * unHighlightedCountries.length)
     ];
 
-  alert(randomElement);
+  showModal("three", randomElement);
 });
 
-fetch("data.json")
-  .then((response) => response.json())
-  .then((json) => (countryData = json))
-  .then(() => {
-    fetch("world.json")
-      .then((response) => response.json())
-      .then((worldData) => {
-        highlightedCountries = new Set(Object.keys(countryData));
-        unHighlightedCountries = worldData.features
-          .map((feature) => feature.properties.name)
-          .filter((name) => !highlightedCountries.has(name));
+// Draw map
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "&copy; OpenStreetMap contributors",
+}).addTo(map);
 
-        L.geoJson(worldData, {
-          style: (feature) => getStyle(feature.properties.name),
-          onEachFeature: (feature, layer) => {
-            const { name, iso_a2 } = feature.properties;
+L.geoJson(worldData, {
+  style: (feature) => getStyle(feature.properties.name),
+  onEachFeature: (feature, layer) => {
+    const { name, iso_a2 } = feature.properties;
 
-            layer.on("mouseover", function () {
-              this.setStyle(getStyle(name, true));
-              infoBox.innerHTML = `<strong>${name}</strong>`;
-              infoBox.classList.remove("invisible");
-            });
+    layer.on("mouseover", function () {
+      this.setStyle(getStyle(name, true));
+      infoBox.innerHTML = `<strong>${name}</strong>`;
+      infoBox.classList.remove("invisible");
+    });
 
-            layer.on("mouseout", function () {
-              this.setStyle(getStyle(name));
-              infoBox.classList.add("invisible");
-            });
+    layer.on("mouseout", function () {
+      this.setStyle(getStyle(name));
+      infoBox.classList.add("invisible");
+    });
 
-            // Click event for displaying country-specific text
-            layer.on("click", function () {
-              if (countryData[name]) {
-                const { content, order } = countryData[name];
-                modalWikiLink.href = `https://en.wikipedia.org/wiki/${name}`;
-                modalContent.innerText = `(${order}: ${name})\n${content}`;
-                modalFlag.src = `https://flagsapi.com/${iso_a2}/flat/64.png`;
-                showModal();
-              }
-            });
-          },
-        }).addTo(map);
-      })
-      .catch((error) => console.error("Error loading countryData:", error));
-  });
+    layer.on("click", function () {
+      if (countryData[name]) {
+        let data = { iso_a2, name, ...countryData[name] };
+        showModal("one", data);
+      }
+    });
+  },
+}).addTo(map);
